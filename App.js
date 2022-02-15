a4bad675b8243be4f53601e07173f10cdfb87023
Nadya Patricia <nadya.patricia@xfers.com>
2022-02-15 21:42:28 +0700

    Create add task section

--- App.js
+++ App.js
@@ -1,5 +1,13 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  KeyboardAvoidingView,
+  Platform,
+  TextInput,
+  TouchableOpacity,
+} from 'react-native';
 import Task from './components/Task';
 
 export default function App() {
@@ -8,10 +16,22 @@ export default function App() {
       <View style={styles.tasksWrapper}>
         <Text style={styles.sectionTitle}>Today's tasks</Text>
 
-        <View style={styles.items}>
+        <View>
           <Task item='Learn maths' />
           <Task item='Shop groceries' />
         </View>
+
+        <KeyboardAvoidingView
+          behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+          style={styles.writeTaskWrapper}
+        >
+          <TextInput style={styles.input} placeholder='Write a task' />
+          <TouchableOpacity>
+            <View style={styles.addWrapper}>
+              <Text>+</Text>
+            </View>
+          </TouchableOpacity>
+        </KeyboardAvoidingView>
       </View>
     </View>
   );
@@ -23,6 +43,7 @@ const styles = StyleSheet.create({
     backgroundColor: '#e8eaed',
   },
   tasksWrapper: {
+    flex: 1,
     paddingTop: 80,
     paddingHorizontal: 20,
   },
@@ -31,5 +52,33 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     marginBottom: 30,
   },
-  items: {},
+  writeTaskWrapper: {
+    position: 'absolute',
+    bottom: 40,
+    left: 20,
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    height: 100,
+  },
+  input: {
+    paddingVertical: 15,
+    paddingHorizontal: 15,
+    backgroundColor: '#FFF',
+    borderRadius: 60,
+    borderColor: '#C0C0C0',
+    borderWidth: 1,
+    width: 250,
+  },
+  addWrapper: {
+    width: 60,
+    height: 60,
+    backgroundColor: '#FFF',
+    borderRadius: 60,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderColor: '#C0C0C0',
+    borderWidth: 1,
+  },
 });
